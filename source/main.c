a1e69e20fe5db867481b9811cb6238a26bd14a39
Aaron Kim <akim106@wch136-24.cs.ucr.edu>
2019-10-09 12:24:05 -0700

    Finalizing Exercise 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: akim106
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 4  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -11,13 +11,95 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#define button PINA & 0x01
+#define LED PORTB
+
+enum LED_States { LED_1, Held_1, LED_2, Held_2 } LED_State;
+
+void TickFct_LED() {
+    switch(LED_State) {
+      case LED_1:
+        if (!button) {
+          LED_State = LED_1;
+          break;
+        } else if (button) {
+          LED_State = Held_1;
+          break;
+        } else {
+          LED_State = LED_1;
+          break;
+        }
+      
+      case Held_1:
+        if (!button) {
+          LED_State = LED_2;
+          break;
+        } else if (button) {
+          LED_State = Held_1;
+          break;
+        } else {
+          LED_State = Held_1;
+          break;
+        }
+
+      case LED_2:
+        if (!button) {
+          LED_State = LED_2;
+          break;
+        } else if (button) {
+          LED_State = Held_2;
+          break;
+        } else {
+          LED_State = LED_2;
+          break;
+        }
+
+      case Held_2:
+        if (!button) {
+          LED_State = LED_1;
+          break;
+        } else if (button) {
+          LED_State = Held_2;
+          break;
+        } else {
+          LED_State = Held_2;
+          break;
+        }
+
+      default:
+        LED_State = LED_1;
+        break;
+    }
+
+    switch (LED_State) {
+      case LED_1:
+        LED = (LED & 0x00) | 0x01;
+        break;
+
+      case Held_1:
+        LED = (LED & 0x00) | 0x01;
+        break;
+
+      case LED_2:
+        LED = (LED & 0x00) | 0x02;
+        break;
+
+      case Held_2:
+        LED = (LED & 0x00) | 0x02;
+        break;
+      
+      default:
+        break; 
+    }
+}
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0x00; //PORT A = inputs
+    DDRB = 0xFF; PORTB = 0x00; //PORT C = outputs
+    LED_State = LED_1;
 
-    /* Insert your solution below */
     while (1) {
-
+      TickFct_LED();      
     }
     return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -26,19 +26,50 @@
 echo ======================================================\n
 echo Running all tests..."\n\n
 
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
+# Add tests below
+test "PINA: 0x01, 0x00 => PORTB: 0x02, 1 Button Press"
+set LED_State = LED_1
+setPINA 0x01
+continue 5
 setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
-continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
+continue 5
+expectPORTB 0x02
 checkResult
 
-# Add tests below
+test "PINA: 0x01 => PORTB: 0x01, Hold Button"
+set LED_State = LED_1
+setPINA 0x01
+continue 10
+expectPORTB 0x01
+checkResult
+
+test "PINA: 0x00 => PORTB: 0x01, No Input"
+set LED_State = LED_1
+setPINA 0x00
+continue 5
+expectPORTB 0x01
+checkResult
+
+test "PINA: 0x01, 0x00 x2 => PORTB: 0x01, 2 Button Presses"
+set LED_State = LED_1
+setPINA 0x01
+continue 5
+setPINA 0x00
+continue 5
+setPINA 0x01
+continue 5
+setPINA 0x00
+continue 5
+expectPORTB 0x01
+checkResult 
+
+test "PINA: 0x01 => PORTB: 0x01, Hold Button"
+set LED_State = LED_1
+setPINA 0x01
+continue 10
+expectPORTB 0x01
+checkResult
+ 
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
